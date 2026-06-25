0171b9ae9ded0bf1edbeee14cca2c589dc3dda2a
Joshua Cadavez <joshua.cadavez@outlook.com>
2026-06-25 14:45:51 -0400

    fix: force service requests reload on component initialization

--- frontend/hickory-lawn-care-services/src/app/features/service-requests/service-request-list/service-request-list.component.ts
+++ frontend/hickory-lawn-care-services/src/app/features/service-requests/service-request-list/service-request-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { DatePipe } from "@angular/common";
 import { ServiceRequestService } from "../../../core/services/service-request.service";
 
@@ -9,10 +9,15 @@ import { ServiceRequestService } from "../../../core/services/service-request.se
     templateUrl: './service-request-list.component.html',
     // styleUrl: './service-request-list.component.css' // TODO: Add styling later
 })
-export class ServiceRequestListComponent {
+export class ServiceRequestListComponent implements OnInit {
     // Inject the service
     private serviceRequestService = inject(ServiceRequestService);
 
     // Expose the resource to the template
     requests = this.serviceRequestService.requestsResource;
+
+    ngOnInit() {
+        // Force a reload of the service requests list when the component loads
+        this.serviceRequestService.requestsResource.reload();
+    }
 }
